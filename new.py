55eccaa5c9eaedee436485401a8119a5e4735b70
Rungbam(main) <marungbam@naver.com>
2023-09-15 14:45:53 +0900

    Drill 1 제출 (2022182039 최정민)

---
+++ new.py
@@ -0,0 +1,33 @@
+import turtle
+
+def right_move() :
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+def left_move() :
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+def up_move() :
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+def down_move() :
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart() :
+    turtle.reset()
+
+turtle.shape('turtle')
+
+turtle.onkey(right_move, 'd')
+turtle.onkey(left_move, 'a')
+turtle.onkey(up_move, 'w')
+turtle.onkey(down_move, 's')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
